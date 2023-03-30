2fcf6045d20c206b4f4078e838f9e8b01865072b
simplyarsa <simplyarsa15@gmail.com>
2023-03-31 02:42:10 +0530

    text changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ import Tile from './Tile.jsx'
 import { AiFillGithub, AiFillLinkedin, AiOutlineMessage, AiOutlinePlus } from 'react-icons/ai';
 import capitalized from './word';
 
+const generateWord = () => {
+  const meow = () => {
+    let random = Math.floor(Math.random() * 7) + 3;
+    return " meow".repeat(random);
+  }
+  let char = ['.', '-', '?', '!'];
+  let random2, string = meow()
+  let random3 = Math.floor(Math.random() * 3) + 1
+  for (let i = 0; i < random3; i++) {
+    random2 = Math.floor(Math.random() * 3)
+    string += (char[random2] + meow())
+  }
+  random2 = Math.floor(Math.random() * 3)
+  string += char[0]
+  const capitalized =
+    string.charAt(1).toUpperCase()
+    + string.slice(2)
+  return capitalized
+}
+
 export default function App() {
 
   const bottomRef = useRef(null);
@@ -27,7 +47,7 @@ export default function App() {
   };
 
   useEffect(() => {
-    setInfo(prev => ({ ...prev, desc: capitalized }))
+    setInfo(prev => ({ ...prev, desc: generateWord() }))
     const getData = async () => {
       try {
         const res = await axios.get("https://api.thecatapi.com/v1/images/search?api_key=live_aZEF4SeBIv5sZnfKDCgauFT9gVDf7R9ftMxEE0FR2Y4rQMbrahJ5ajYBZhJyb0Ek")
